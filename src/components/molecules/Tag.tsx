2180fec5e26f00dfc7bd26b6444147704161663e
Elmer Raúl Escandón Tufino <elmer.escandon@utec.edu.pe>
2024-04-14 13:10:58 -0500

    Refactor Tag component to handle different types of users

--- src/components/molecules/Tag.tsx
+++ src/components/molecules/Tag.tsx
@@ -1,13 +1,28 @@
+import Image from "astro/components/Image.astro";
 import React from "react";
 
 type TagProps = {
     user: string;
     imageURL: string;
+    type: "chatbot" | "user";
 };
 
-const Tag = ({user, imageURL}) => {
-    return;
-    <div></div>;
+const Tag = ({user, imageURL, type}: TagProps) => {
+    if (type === "chatbot") {
+        return (
+            <div>
+                <Image src={imageURL} alt={user} width={100} height={100} />
+                <p>{user}</p>
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <Image src={imageURL} alt={user} width={100} height={100} />
+                <p>{user}</p>
+            </div>
+        );
+    }
 };
 
 export default Tag;
