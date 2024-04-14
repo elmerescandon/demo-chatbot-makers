4766cd5d54058d1e1d3a6c24481186b495d6091b
Elmer Raúl Escandón Tufino <elmer.escandon@utec.edu.pe>
2024-04-14 13:20:52 -0500

    Refactor Tag component to handle different types of users and update styling

--- src/components/molecules/Tag.tsx
+++ src/components/molecules/Tag.tsx
@@ -10,14 +10,18 @@ type TagProps = {
 const Tag = ({user, imageURL, type}: TagProps) => {
     if (type === "chatbot") {
         return (
-            <div>
-                <Image src={imageURL} alt={user} width={100} height={100} />
+            <div className="flex gap-2 items-center">
+                <img
+                    src={imageURL}
+                    alt={user}
+                    className="w-4 h-4 rounded-full"
+                />
                 <p>{user}</p>
             </div>
         );
     } else {
         return (
-            <div>
+            <div className="flex gap-2 items-center">
                 <Image src={imageURL} alt={user} width={100} height={100} />
                 <p>{user}</p>
             </div>
