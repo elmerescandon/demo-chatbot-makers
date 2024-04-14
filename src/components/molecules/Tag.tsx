8e94eb6b75d59aa407ab83fed530abb4624820ea
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-04-14 12:57:54 -0500

    :sparkles: Add some components

---
+++ src/components/molecules/Tag.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type TagProps = {
+    user: string;
+    imageURL: string;
+};
+
+const Tag = ({user, imageURL}) => {
+    return;
+    <div></div>;
+};
+
+export default Tag;
