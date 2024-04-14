9a78395e5f16e156411df9093463f6cd7d94017c
Elmer Raúl Escandón Tufino <elmer.escandon@utec.edu.pe>
2024-04-14 16:07:47 -0500

    Refactor Loading component CSS to improve animation and aspect ratio

--- src/components/molecules/Loading/Loading.tsx
+++ src/components/molecules/Loading/Loading.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./Loading.css";
 
 const Loading = () => {
-    return <div className="loader"></div>;
+    return (
+        <div className="px-2 py-1 bg-neutral-100 w-12 rounded-lg">
+            <div className="loader w-8 h-5"></div>
+        </div>
+    );
 };
 
 export default Loading;
